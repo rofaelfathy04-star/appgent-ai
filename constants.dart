2d061854dff938796d8247ce461ed58af24263da
rofaelfathy04-star <rofaelfathy04@gmail.com>
2026-05-16 20:23:33 +0300

    Delete constants.dart

--- constants.dart
+++
@@ -1,4 +0,0 @@
-class AppConstants {
-  static const String openAiApiKey = String.fromEnvironment('OPENAI_API_KEY');
-  static const String openAiEndpoint = "https://api.openai.com/v1/chat/completions";
-}
